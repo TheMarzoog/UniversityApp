1af853b7589465ee6bca17e4437164c0099dca75
marzoogtech <marzoog.tech@gmail.com>
2019-07-24 16:25:09 +0300

    setOnClickListener

--- app/src/main/java/tech/marzoog/universityapp/MainActivity.java
+++ app/src/main/java/tech/marzoog/universityapp/MainActivity.java
@@ -2,12 +2,27 @@ package tech.marzoog.universityapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userIDFild;
+    private Button loginButton;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userIDFild = findViewById(R.id.userID);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener((v) -> {
+            String usrID = userIDFild.getText().toString();
+            Toast.makeText(MainActivity.this, usrID, Toast.LENGTH_LONG).show();
+        });
     }
 }
