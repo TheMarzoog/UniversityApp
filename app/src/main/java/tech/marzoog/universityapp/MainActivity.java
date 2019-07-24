d99ff30d98e5548ea0f5eccd5cfe8572f8e64bc9
Administrator <marzoog.tech@gmail.com>
2019-07-24 17:38:23 +0300

    add sign up activity

--- app/src/main/java/tech/marzoog/universityapp/MainActivity.java
+++ app/src/main/java/tech/marzoog/universityapp/MainActivity.java
@@ -1,12 +1,14 @@
 package tech.marzoog.universityapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText userIDFild;
     private Button loginButton;
@@ -19,10 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
         userIDFild = findViewById(R.id.userID);
         loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
 
-        loginButton.setOnClickListener((v) -> {
-            String usrID = userIDFild.getText().toString();
-            Toast.makeText(MainActivity.this, usrID, Toast.LENGTH_LONG).show();
-        });
+//        loginButton.setOnClickListener((v) -> {
+//            String usrID = userIDFild.getText().toString();
+//            Toast.makeText(MainActivity.this, usrID, Toast.LENGTH_LONG).show();
+//        });
+    }
+    public void onClick(View v)
+    {
+        //check if user & password field are not empty
+        //check db if user exist, login, otherwise return error. PHP - Hassan
+        Course_list();
+    }
+    public void Course_list()
+    {
+        Intent intent = new Intent(this, Course_list.class);
+        startActivity(intent);
     }
 }
